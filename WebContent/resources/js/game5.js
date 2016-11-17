dbc5607473eef04e5b352df17d6e5599a90c501e
hwyang <hongwei_yang1990@hotmail.com>
2016-11-17 14:50:45 +0100

    function to get Level through ajax created loading player, balls and background through level object

--- WebContent/resources/js/game5.js
+++ WebContent/resources/js/game5.js
@@ -13,14 +13,18 @@ var groundSprite;
 
 function preload() {
 	game.load.image('starfield', '../resources/assets/misc/starfield.jpg');
-	game.load.image('ground_invisible', '../resources/images/dak_invisible.png');
+	game.load
+			.image('ground_invisible', '../resources/images/dak_invisible.png');
 	game.load.image('ground', '../resources/images/cartoon-roof.jpg');
 	game.load.image('projectile', '../resources/images/xmas-harpoon.png')
-	game.load.spritesheet('santa', '../resources/images/santa.png', 91,	118);
-	
-	game.load.image('snowball_16', '../resources/images/snowballs/snowball_16.png');
-    game.load.image('snowball_32', '../resources/images/snowballs/snowball_32.png');
-    game.load.image('snowball_48', '../resources/images/snowballs/snowball_48.png');
+	game.load.spritesheet('santa', '../resources/images/santa.png', 91, 118);
+
+	game.load.image('snowball_16',
+			'../resources/images/snowballs/snowball_16.png');
+	game.load.image('snowball_32',
+			'../resources/images/snowballs/snowball_32.png');
+	game.load.image('snowball_48',
+			'../resources/images/snowballs/snowball_48.png');
 }
 
 var player;
@@ -29,8 +33,9 @@ var projectilesInUse = 0;
 var projectiles;
 
 var cachedLevel;
-var levelUrl = '../resources/levels/';
-var level;
+var levelUrl = '../resources/levels/level';
+var levelExtension = '.json'
+var level = 1;
 
 var platforms;
 var bounds;
@@ -47,82 +52,68 @@ var projectileStartLocationY;
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	projectileStartLocationY = game.world.height - 100
-	
-	space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    space.onDown.add(useProjectile, this);
 
-	// Background
-	background = game.add.tileSprite(0, 0, width, height, 'starfield');
-	groundSprite = game.add.tileSprite(0, game.world.height - 32, width, 32, 'ground');
+	groundSprite = game.add.tileSprite(0, game.world.height - 32, width, 32,
+			'ground');
 
 	// Platforms
 	platforms = game.add.group();
 	platforms.enableBody = true;
-	
+
 	bounds = game.add.group();
 	bounds.enableBody = true;
 
 	floor = platforms.create(0, game.world.height - 32, 'ground_invisible');
 	floor.body.immovable = true;
-	
+
 	var top = bounds.create(0, -32, 'ground_invisible');
 	top.body.immovable = true;
 
-	// Player
-	player = game.add.sprite(32, game.world.height - 300, 'santa');
-	game.physics.arcade.enable(player);
-
-	player.body.bounce.y = 0;
-	player.body.gravity.y = 1500;
-	player.body.collideWorldBounds = true;
-
-	player.animations.add('right', [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 20, true);
-	player.animations.add('left', [ 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], 20, true);
-
 	cursors = game.input.keyboard.createCursorKeys();
 
-	//Balls
+	// Balls
 	balls = game.add.group();
-    balls.enableBody = true;
-    
-    balls.add(new Ball(1, 200));
-	
+	balls.enableBody = true;
+
 	// Score
-	scoreText = scoreText = game.add.text(width / 2, 16, 'score: 0', {
+	scoreText = scoreText = game.add.text(10,10, 'level: '+level+'/4', {
 		fontSize : '32px',
 		fill : '#ffffff'
 	});
-	scoreText.anchor.setTo(0.5, 0.5);
-	
+
 	// projectiles
 	projectiles = game.add.group();
 	projectiles.enableBody = true;
+
+	loadLevel(getLevel(level));
 }
 
 function useProjectile() {
-	if(projectilesInUse < numOfProjectiles) {
-		let startLocationX = player.body.x + (player.width/4);
-		var projectile = projectiles.create(startLocationX, projectileStartLocationY, 'projectile');
-		projectilesInUse++; 
+	if (projectilesInUse < numOfProjectiles) {
+		let startLocationX = player.body.x + (player.width / 4);
+		var projectile = projectiles.create(startLocationX,
+				projectileStartLocationY, 'projectile');
+		projectilesInUse++;
 		game.world.bringToTop(groundSprite);
 	}
 }
 
 function update() {
-	//Collisions
+	// Collisions
 	game.physics.arcade.collide(player, platforms);
 	game.physics.arcade.collide(projectiles, platforms);
 	game.physics.arcade.collide(projectiles, bounds);
-	
-	game.physics.arcade.overlap(projectiles, bounds, destroyProjectile, null, this);
-	game.physics.arcade.overlap(projectiles, balls, projectileBallCollision, null, this);
-	
+
+	game.physics.arcade.overlap(projectiles, bounds, destroyProjectile, null,
+			this);
+	game.physics.arcade.overlap(projectiles, balls, projectileBallCollision,
+			null, this);
+
 	handleBallPhysics();
-	
-    handlePlayerMovement();
-    
-    //Projectile
-    for (var i = 0; i < projectiles.children.length; i++) {
+	handlePlayerMovement();
+
+	// Projectile
+	for (var i = 0; i < projectiles.children.length; i++) {
 		var projectile = projectiles.children[i];
 		projectile.body.y -= 7.35;
 	}
@@ -133,31 +124,35 @@ function destroyProjectile(projectile) {
 	projectilesInUse--;
 }
 
-function projectileBallCollision(projectile, ball){
+function projectileBallCollision(projectile, ball) {
 	destroyProjectile(projectile);
-	if(ball.key === 'snowball_48'){
+	if (ball.key === 'snowball_48') {
 		splitBall(ball, 2);
-	} else if(ball.key === 'snowball_32'){
+	} else if (ball.key === 'snowball_32') {
 		splitBall(ball, 1);
 	}
 	ball.destroy();
-	if(balls.children.length === 0){
+	if (balls.children.length === 0) {
 		window.alert("Level complete");
 	}
 }
 
-function splitBall(original, newBallSize){
-	let velocityX = original.body.velocity.x;
-	let velocityY = original.body.velocity.y;
-	balls.add(new Ball(newBallSize, velocityX, velocityY, original.body.x, original.body.y));
-	balls.add(new Ball(newBallSize, -velocityX, velocityY, original.body.x, original.body.y));
+function splitBall(original, newBallSize) {
+	let
+	velocityX = original.body.velocity.x;
+	let
+	velocityY = original.body.velocity.y;
+	balls.add(new Ball(newBallSize, velocityX, velocityY, original.body.x,
+			original.body.y));
+	balls.add(new Ball(newBallSize, -velocityX, velocityY, original.body.x,
+			original.body.y));
 }
 
-function handleBallPhysics(){
-	if(game.physics.arcade.collide(balls, floor)){
+function handleBallPhysics() {
+	if (game.physics.arcade.collide(balls, floor)) {
 		for (var i = 0; i < balls.children.length; i++) {
 			var ball = balls.children[i];
-			if(ball.body.touching.down){
+			if (ball.body.touching.down) {
 				ball.body.velocity.y = -900;
 			}
 		}
@@ -165,41 +160,101 @@ function handleBallPhysics(){
 	game.physics.arcade.collide(balls, platforms);
 }
 
-function loadLevel(levelName){
+function loadLevel(levelName) {
 	window.alert('Loading ' + levelName);
 }
 
-function handlePlayerMovement(){
-	player.body.velocity.x = 0;
-
-    if (cursors.left.isDown){
-        player.body.velocity.x = -250;
-        player.animations.play('left');
-    } else if (cursors.right.isDown){
-        player.body.velocity.x = 250;
-        player.animations.play('right');
-    } else{
-        player.animations.stop();
-        player.frame = 32;
-    }
+function handlePlayerMovement() {
+	if (!!player) {
+		player.body.velocity.x = 0;
+		if(game.input.activePointer.isDown && player.body.touching.down) {
+			useProjectile();
+		}
+		
+		if (cursors.left.isDown) {
+			player.body.velocity.x = -250;
+			player.animations.play('left');
+		} else if (cursors.right.isDown) {
+			player.body.velocity.x = 250;
+			player.animations.play('right');
+		} else {
+			player.animations.stop();
+			player.frame = 32;
+		}
+	}
 }
 
 function getLevel(levelName) {
-	let loadedLevel;
-	let jqXHR = $.ajax({
-		dataType: "json",
-		async: false,
-		url: (levelUrl + levelName),
-		  beforeSend: function( xhr ) {
-		    xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
-		  }
-		}).done(function(data) {
-			loadedLevel = data;
-		});
-	return loadedLevel;
+	let
+	levelObject;
+	let
+	jqXHR = $.ajax({
+		dataType : "json",
+		async : false,
+		url : (levelUrl + levelName + levelExtension),
+		beforeSend : function(xhr) {
+			xhr.overrideMimeType("text/plain; charset=x-user-defined");
+		}
+	}).done(function(data) {
+		levelObject = data;
+	});
+	return levelObject;
 }
 
 function loadLevel(levelObject) {
-	
+	let
+	levelBalls = levelObject.balls;
+	let
+	platforms = levelObject.platforms;
+	let
+	playerPosition = levelObject.player.position;
+	let
+	backgroundName = levelObject.background;
+
+	// destroy background and create new background
+	createBackground(backgroundName);
+
+	// destroy current balls and create new balls
+	createBalls(levelBalls);
+
+	// destroy player and create new player object
+	createPlayer(playerPosition);
 }
 
+function createPlayer(playerPosition) {
+	if (!!player) {
+		player.destroy();
+	}
+	player = game.add.sprite(playerPosition.x, playerPosition.y, 'santa');
+	game.physics.arcade.enable(player);
+	player.body.bounce.y = 0;
+	player.body.gravity.y = 1500;
+	player.body.collideWorldBounds = true;
+	player.animations.add('right', [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+			13, 14, 15 ], 20, true);
+	player.animations.add('left', [ 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+			27, 28, 29, 30, 31 ], 20, true);
+}
+
+function createBalls(levelBalls) {
+	let
+	i = balls.children.length;
+	while (i--) {
+		balls.children[i].destroy();
+	}
+
+	for (i = 0; i < levelBalls.length; i++) {
+		let
+		ball = levelBalls[i];
+		balls.add(new Ball(ball.size, ball.velocity));
+	}
+
+}
+
+function createBackground(backgroundName) {
+	if (!!background) {
+		background.destroy();
+	}
+	background = game.add.tileSprite(0, 0, width, height, backgroundName);
+	game.world.sendToBack(background);
+}
