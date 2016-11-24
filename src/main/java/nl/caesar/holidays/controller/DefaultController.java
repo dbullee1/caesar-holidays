1fe65184cbf817fa47b89d414c16b2b252a5c893
hwyang <hongwei_yang1990@hotmail.com>
2016-11-24 10:30:20 +0100

    setting cookie behavior changed to always setting when given the requestparameter

--- src/main/java/nl/caesar/holidays/controller/DefaultController.java
+++ src/main/java/nl/caesar/holidays/controller/DefaultController.java
@@ -1,7 +1,6 @@
 package nl.caesar.holidays.controller;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
@@ -11,17 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import nl.caesar.holidays.game.model.Branch;
-import nl.caesar.holidays.game.model.CookieUtil;
 
 @Controller
 @RequestMapping("/index")
 public class DefaultController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String getIndexPage(HttpServletRequest request, HttpServletResponse response,
+	public String getIndexPage(HttpServletResponse response,
 			@RequestParam(value = "branch", required = false) String theme) {
-		Cookie branch = CookieUtil.getCookie(request, "branch");
-		if (branch == null || branch.getValue().equals(Branch.DEFAULT.name())) {
+		if (theme != null) {
 			Branch modelTheme = null;
 			if (!StringUtils.isEmpty(theme)) {
 				try {
--- src/test/java/nl/caesar/holidays/controller/DefaultControllerTest.java
+++ src/test/java/nl/caesar/holidays/controller/DefaultControllerTest.java
@@ -1,12 +1,8 @@
 package nl.caesar.holidays.controller;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import javax.servlet.http.Cookie;
 
 import org.junit.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import nl.caesar.holidays.game.model.Branch;
@@ -18,54 +14,17 @@ public class DefaultControllerTest {
 		DefaultController controller = new DefaultController();
 		// Tenders
 		MockHttpServletResponse tenderResponse = new MockHttpServletResponse();
-		MockHttpServletRequest tenderRequest = new MockHttpServletRequest();
-		controller.getIndexPage(tenderRequest, tenderResponse, "Tenders");
+		controller.getIndexPage(tenderResponse, "Tenders");
 		assertEquals(Branch.TENDERS.name(), tenderResponse.getCookie("branch").getValue());
 
 		// Experts
 		MockHttpServletResponse expertsResponse = new MockHttpServletResponse();
-		MockHttpServletRequest expertsRequest = new MockHttpServletRequest();
-		controller.getIndexPage(expertsRequest, expertsResponse, "Experts");
+		controller.getIndexPage(expertsResponse, "Experts");
 		assertEquals(Branch.EXPERTS.name(), expertsResponse.getCookie("branch").getValue());
 
 		// Anything else
 		MockHttpServletResponse defaultResponse = new MockHttpServletResponse();
-		MockHttpServletRequest defaultRequest = new MockHttpServletRequest();
-		assertEquals("index", controller.getIndexPage(defaultRequest, defaultResponse, "asdas"));
+		assertEquals("index", controller.getIndexPage(defaultResponse, "asdas"));
 		assertEquals(Branch.DEFAULT.name(), defaultResponse.getCookie("branch").getValue());
-	}
-
-	@Test
-	public void testPreviousCookieSet() {
-		DefaultController controller = new DefaultController();
-		Cookie[] cookies = new Cookie[1];
-		cookies[0] = new Cookie("branch", Branch.EXPERTS.name());
-
-		// Tenders
-		MockHttpServletResponse tenderResponse = new MockHttpServletResponse();
-		MockHttpServletRequest tenderRequest = new MockHttpServletRequest();
-		tenderRequest.setCookies(cookies);
-		controller.getIndexPage(tenderRequest, tenderResponse, "Tenders");
-		// not setting cookie because there was already a cookie set on the
-		// request
-		assertTrue(tenderResponse.getCookie("branch") == null);
-
-		// Experts
-		MockHttpServletResponse expertsResponse = new MockHttpServletResponse();
-		MockHttpServletRequest expertsRequest = new MockHttpServletRequest();
-		expertsRequest.setCookies(cookies);
-		controller.getIndexPage(expertsRequest, expertsResponse, "Experts");
-		// not setting cookie because there was already a cookie set on the
-		// request
-		assertTrue(expertsResponse.getCookie("branch") == null);
-
-		// Anything else
-		MockHttpServletResponse defaultResponse = new MockHttpServletResponse();
-		MockHttpServletRequest defaultRequest = new MockHttpServletRequest();
-		defaultRequest.setCookies(cookies);
-		assertEquals("index", controller.getIndexPage(defaultRequest, defaultResponse, "asdas"));
-		// not setting cookie because there was already a cookie set on the
-		// request
-		assertTrue(defaultResponse.getCookie("branch") == null);
 	}
 }
